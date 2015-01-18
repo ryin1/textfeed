0cda11856b98c2ec9f429f26076c53969280a095
ryin1 <ryin+1@seas.upenn.edu>
2015-01-17 23:48:28 -0500

    Update app.py

--- hackpack/app.py
+++ hackpack/app.py
@@ -33,7 +33,7 @@ def sms():
     if "everyblock" in body:
         output = body.replace('everyblock','')
         response.sms("You called 'Everyblock' API on: %s!"%output)
-        r = requests.get('https://api.everyblock.com/content/')
+        r = requests.get('https://api.everyblock.com/content/', headers = {'Authorization' : 'Token fc51e71739c072154f4f8d58ed4f9ec0770aee76'})
     else:
         response.sms("You did not call 'Everyblock' API.")
     return str(response)
