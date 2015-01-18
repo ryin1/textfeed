d03b191f3a4d71f02edb4c3af5b88a75d531e68d
Raymond Yin <ryin@seas.upenn.edu>
2015-01-17 21:22:32 -0500

    Update app.py
    'get' in methods

--- hackpack/app.py
+++ hackpack/app.py
@@ -26,7 +26,7 @@ def voice():
 
 
 # SMS Request URL
-@app.route('/sms', methods=['POST'])
+@app.route('/sms', methods=['GET', 'POST'])
 def sms():
     response = twiml.Response()
     body = request.form['Body']
